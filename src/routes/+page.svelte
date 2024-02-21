ec67f6878ad8918257ef5643e994183e81d67339
Ozkung <Pongpop.suw@gmail.com>
2024-02-21 12:04:41 +0700

    set Project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<div class="container h-full mx-auto flex items-center">
+	<div class="space-y-5">
+		<!-- <h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
+			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
+		</ul> -->
+		<form action="" method="post">
+			<div class="flex items-center flex-wrap" style="width: 100%">
+				<div class="mt-4 mx-3 mx-auto">
+					<img src="Snaptech.png" width="100%"/>
+				</div>
+				<div style="width: 100%" class="mt-4">
+					<div class="flex justify-center items-center">
+						<div>
+							<input class="input" style="width: 100%" type="text" placeholder="Email Address" />
+						</div>
+						<div>
+							<button type="button" class="m-4 btn variant-filled">Send</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
